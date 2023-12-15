1b36833b4535c708de627f2aa22e911f587232b7
Yusuf Can Yüce <113469256+rihenn@users.noreply.github.com>
2023-12-15 03:19:17 +0300

    home frontend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Yogahonma</title>
+</head>
+
+<body>
+    <header class="bg-transparent fixed-top" id="header">
+        <div class="d-flex col-12" name="logo">
+            <a href="#">
+                <img src="logo.png" class="logo-header" style="margin:0;">
+            </a>
+            <div class="row col-11 justify-content-end align-items-start mt-2">
+
+                <div class="d-flex col-11 col-lg-auto mb-2 mb-lg-0  justify-content-end">
+                    <a href="#" class="text-decoration-none color4 txt2 text-end me-2">home</a>
+                    <a href="#" class="text-decoration-none text-end me-2"><img src="shop-icon.png"
+                            alt="mağaza"></a>
+                    <a href="#" class="text-decoration-none color4 txt2 text-end me-2">login</a>
+
+                    <form class="d-flex col-11 col-lg-auto mb-2 mb-lg-0  " role="search">
+                        <input type="search" class="form-control form-control-dark text-bg-light search"
+                            placeholder="Ara..." aria-label="Search">
+                    </form>
+                </div>
+                <div class="d-flex justify-content-end align-items-center " style="height: 1.5rem">
+
+                    <div class="d-flex col justify-content-start">
+                        <a href="#" class="text-decoration-none txt color4 text-start ms-4 me-3">Yoga</a>
+                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Plates</a>
+                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Tedavi</a>
+                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
+                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Öğretmenler</a>
+                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
+                    </div>
+
+
+                    <a href="#" class="btn bg-color4 color1 text-center me-2">Kitaplar</a>
+                    <a href="#" class="btn bg-color4 color1 text-center">Ücretlendirme</a>
+
+                </div>
+            </div>
+
+        </div>
+    </header>
+
+    <div class="row col-12   m-0 p-0">
+
+
+        <div id="carouselExampleCaptions" class="carousel slide m-0 p-0">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner ">
+                <div class="carousel-item active">
+                    <img src="plates1.jpg" class="d-block w-100 carousel-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2>First slide label</h2>
+                        <h3>Some representative placeholder content for the third slide.</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="plates2.jpg" class="d-block w-100 carousel-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2>Second slide label</h2>
+                        <h3>Some representative placeholder content for the third slide.</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="plates3.jpg" class="d-block w-100 carousel-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2>Third slide label</h2>
+                        <h3>Some representative placeholder content for the third slide.</h3>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+
+        </div>
+    </div>
+    <div class="row m-4 p-4">
+        <div class="d-flex col justify-content-center m-4 p 4">
+            <h1 class="text-center">Londra Yoga</h1>
+        </div>
+        <hr class="my-4">
+        <p class="home-info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, ex fugiat, perspiciatis
+            veniam quisquam deleniti voluptates id magni autem asperiores eaque repudiandae quibusdam error, nam
+            explicabo sit nobis? Provident, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium, impedit praesentium hic est libero. Nesciunt dicta exercitationem, blanditiis,
+            provident rerum quod itaque necessitatibus magnam, odio porro nam. Saepe, fugiat? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Eligendi, necessitatibus quo eius ab cumque optio perspiciatis. Accusamus
+            pariatur veniam molestias odio esse, nesciunt doloribus optio quam obcaecati deleniti nemo voluptates?
+            Voluptatem officiis quos cum vero, alias laborum deserunt incidunt nihil eligendi dolore quae suscipit eius
+            quidem. Ex, eius? Veniam reprehenderit vero nisi. Earum, quaerat! Facere libero ab natus odit repudiandae.
+            Cupiditate, obcaecati vero! Modi neque culpa quod voluptas officia earum delectus rem doloremque, voluptate
+            ipsam illum repellendus, sequi repudiandae, error ullam vitae sapiente reprehenderit? Consectetur facere
+            rerum provident quidem voluptate.
+            Deleniti pariatur similique et ipsum iure, aperiam quibusdam eligendi beatae ullam quas cupiditate modi!
+            Dolore incidunt similique rerum est natus, facilis totam sed nobis? Consequuntur obcaecati architecto
+            perferendis cupiditate at?</p>
+        <p class="home-info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo eum et quam voluptatibus
+            aut, molestias fugit, aspernatur voluptas, hic debitis distinctio mollitia ab quibusdam tempore. Eligendi
+            consequuntur optio ex ab?Quis tenetur sint beatae illum tempora non doloremque nihil corrupti ea quos quo
+            reprehenderit fuga, saepe dolorem rem voluptatum iste amet, blanditiis temporibus inventore neque
+            laudantium? Aperiam aliquam in explicabo.</p>
+        <p class="home-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ratione soluta nobis quidem
+            placeat saepe, quasi doloremque ullam similique atque porro dolore ut aliquid corrupti ducimus corporis odio
+            perferendis molestiae!</p>
+        <div class="d-flex col justify-content-center m-4 p 4">
+            <a href="" class="btn bg-color1 color4 ">Atölyeyi Keşfet</a>
+        </div>
+
+
+    </div>
+    <div class="row m-4 p-4 col justify-content-center">
+        <div class="d-flex col justify-content-center m-4 p 4">
+            <h1 class="text-center">Herkes Yoga Yapabilir</h1>
+        </div>
+        <hr class="my-4">
+        <div class="card m-4" style="width: 18rem;">
+            <img src="add.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title text-center color1">Card title</h4>
+                <p class="card-text text-center color1">Some quick example text to build on the card title and make up
+                    the bulk of the card's content.</p>
+                <a href="#" class="btn bg-color3 color2 ms-5 card-btn">Go somewhere</a>
+            </div>
+        </div>
+
+
+        <div class="card m-4" style="width: 18rem;">
+            <img src="lotus.png" class="card-img-top" alt="...">
+            <div class="card-body ">
+                <h4 class="card-title text-center color1">Card title</h4>
+                <p class="card-text text-center color1">Some quick example text to build on the card title and make up
+                    the bulk of the card's content.</p>
+                <a href="#" class="btn bg-color3 color2 ms-5 card-btn">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="d-flex col justify-content-center">
+        <img src="plates4.jpg" style="width: 60%" alt="">
+    </div>
+
+    <div class="foother login">
+        <div class="row col  aling-item-start ms-4 ">
+            <div class="flex col-5 justify-content-start ">
+                <h2>EN YENİ GELİŞMELER</h2>
+                <h4>için kaydolun size haber verelim en yeni gelişmelerden haberdar olmak için...</h4>
+            </div>
+
+            <div class="flex col-6 justify-content-end ms-4">
+                <div class="flex col justify-content-end ms-4">
+                    <form action="#">
+                        <input type="text" class="row text-center login-input" placeholder="your name">
+
+                        <input type="text" class="row text-center login-input  mt-4" placeholder="your mail">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr class="my-4">
+
+    <div class="row col foother-margin">
+        <div class="flex col-8">
+        <h3>HAKKIMIZDA</h3>
+        <H5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, facilis maiores! Sed tempore corrupti sunt reprehenderit, odio aliquam similique obcaecati accusantium minima nam. Dolore, harum dolorum nobis voluptatem quae assumenda.
+        Numquam nesciunt blanditiis dolores fuga, accusamus, laudantium at ab reprehenderit, est animi sapiente vel non modi tempore repellendus? Officia labore eius repellendus consequuntur recusandae perspiciatis numquam dicta hic libero nisi.
+        Laboriosam nihil alias enim sed. Dolores ab temporibus delectus fuga in, dignissimos necessitatibus harum, ipsam nemo laboriosam maiores magni, aperiam commodi quasi iusto quo? Voluptatibus eos ut sed atque aliquid.
+        Repudiandae architecto mollitia atque. Consectetur iste nesciunt ipsum, inventore delectus quidem minus excepturi ad quia, quisquam voluptatum velit enim a hic deserunt voluptatibus sed! Nostrum minus voluptatum iure vel qui.</H5></div>
+        <div class="flex col-3">
+            <div class="d-flex ms-4 mt-4">
+            <a href="" class="btn btn-foother"><img src="instagram.png" alt=""></a>
+            <a href="" class="btn btn-foother"><img src="facebook-app-symbol.png" alt=""></a>
+            <a href="" class="btn btn-foother"><img src="youtube.png" alt=""></a>
+        </div>
+        <div class="d-flex">
+           <img src="logo.png" style="width: 400px;height: auto"  alt="">
+        </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var navbar = $("#header");
+
+            $(window).scroll(function() {
+
+                if ($(this).scrollTop() > 50) {
+                    navbar.addClass("bg-color1");
+                    navbar.removeClass("bg-transparent");
+                } else {
+                    navbar.addClass("bg-transparent");
+                    navbar.removeClass("bg-color1");
+                }
+            });
+        });
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
