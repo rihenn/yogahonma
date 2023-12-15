6c7e41d1ed90a64c9f6700faff4b4525fb502339
Yusuf Can Yüce <113469256+rihenn@users.noreply.github.com>
2023-12-16 01:46:29 +0300

    efetler ve arama yerleri düzenlendi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,99 +7,115 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
 
     <title>Yogahonma</title>
 </head>
 
 <body>
-    <header class="bg-transparent fixed-top" id="header">
+    <div id="searchCard">
+        <div class="card1" style="display: none;" id="card">
+            <div class="card-body ">
+                <div class="d-flex col justify-content-center aling-items-center" style="height: 7rem">
+                <input type="search" class="text-center aling-items-center m-4 bg-color1" style="width: 60%;outline: none;border:none;border-bottom: 3px #4FA095 solid; font-size: 32px;color:#BAD1C2;font-family: Arial, Helvetica, sans-serif" placeholder="Ara...">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <header class="bg-transparent fixed-top header" id="header">
         <div class="d-flex col-12" name="logo">
             <a href="#">
                 <img src="logo.png" class="logo-header" style="margin:0;">
             </a>
-            <div class="row col-11 justify-content-end align-items-start mt-2">
+            <div class="d-flex col-11 align-items-center mt-2">
 
-                <div class="d-flex col-11 col-lg-auto mb-2 mb-lg-0  justify-content-end">
-                    <a href="#" class="text-decoration-none color4 txt2 text-end me-2">home</a>
-                    <a href="#" class="text-decoration-none text-end me-2"><img src="shop-icon.png"
-                            alt="mağaza"></a>
-                    <a href="#" class="text-decoration-none color4 txt2 text-end me-2">login</a>
 
-                    <form class="d-flex col-11 col-lg-auto mb-2 mb-lg-0  " role="search">
-                        <input type="search" class="form-control form-control-dark text-bg-light search"
-                            placeholder="Ara..." aria-label="Search">
-                    </form>
+                <div class="d-flex col-10 justify-content-start">
+                    <a href="#" class="text-decoration-none txt color4 text-start ms-4 me-3">Yoga</a>
+                    <a href="#" class="text-decoration-none txt color4 text-start me-3">Plates</a>
+                    <a href="#" class="text-decoration-none txt color4 text-start me-3">Tedavi</a>
+                    <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
+                    <a href="#" class="text-decoration-none txt color4 text-start me-3">Öğretmenler</a>
+                    <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
                 </div>
-                <div class="d-flex justify-content-end align-items-center " style="height: 1.5rem">
 
-                    <div class="d-flex col justify-content-start">
-                        <a href="#" class="text-decoration-none txt color4 text-start ms-4 me-3">Yoga</a>
-                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Plates</a>
-                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Tedavi</a>
-                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
-                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Öğretmenler</a>
-                        <a href="#" class="text-decoration-none txt color4 text-start me-3">Atölye+Kurs</a>
+
+                <div class="d-flex col-2 justify-content-end">
+                    <a href="#"><img src="shop-icon.png" width="32" height="32" class=" me-3"></a>
+                    <a href="#" id="search"><img src="search.png" width="32" height="32" class=" me-3"></a>
+
+                    <div class="dropdown text-end">
+                        <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="user.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                        </a>
+                        <ul class="dropdown-menu text-small shadow">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        </ul>
                     </div>
-
-
-                    <a href="#" class="btn bg-color4 color1 text-center me-2">Kitaplar</a>
-                    <a href="#" class="btn bg-color4 color1 text-center">Ücretlendirme</a>
-
                 </div>
+
             </div>
 
         </div>
     </header>
 
-    <div class="row col-12   m-0 p-0">
 
 
-        <div id="carouselExampleCaptions" class="carousel slide m-0 p-0">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+    <div class="carousel-inner ">
+        <div class="carousel-item active">
+            <img src="plates1.jpg" class="d-block w-100 carousel-img" alt="...">
+
+            <div class="carousel-caption d-none d-md-block">
+                <h2>First slide label</h2>
+                <h3>Some representative placeholder content for the third slide.</h3>
             </div>
-            <div class="carousel-inner ">
-                <div class="carousel-item active">
-                    <img src="plates1.jpg" class="d-block w-100 carousel-img" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h2>First slide label</h2>
-                        <h3>Some representative placeholder content for the third slide.</h3>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="plates2.jpg" class="d-block w-100 carousel-img" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h2>Second slide label</h2>
-                        <h3>Some representative placeholder content for the third slide.</h3>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="plates3.jpg" class="d-block w-100 carousel-img" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h2>Third slide label</h2>
-                        <h3>Some representative placeholder content for the third slide.</h3>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-
         </div>
+        <div class="carousel-item">
+            <img src="plates2.jpg" class="d-block w-100 carousel-img" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h2>Second slide label</h2>
+                <h3>Some representative placeholder content for the third slide.</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="plates3.jpg" class="d-block w-100 carousel-img" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h2>Third slide label</h2>
+                <h3>Some representative placeholder content for the third slide.</h3>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+
+    </div>
     </div>
     <div class="row m-4 p-4">
         <div class="d-flex col justify-content-center m-4 p 4">
@@ -189,25 +205,32 @@
 
     <div class="row col foother-margin">
         <div class="flex col-8">
-        <h3>HAKKIMIZDA</h3>
-        <H5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, facilis maiores! Sed tempore corrupti sunt reprehenderit, odio aliquam similique obcaecati accusantium minima nam. Dolore, harum dolorum nobis voluptatem quae assumenda.
-        Numquam nesciunt blanditiis dolores fuga, accusamus, laudantium at ab reprehenderit, est animi sapiente vel non modi tempore repellendus? Officia labore eius repellendus consequuntur recusandae perspiciatis numquam dicta hic libero nisi.
-        Laboriosam nihil alias enim sed. Dolores ab temporibus delectus fuga in, dignissimos necessitatibus harum, ipsam nemo laboriosam maiores magni, aperiam commodi quasi iusto quo? Voluptatibus eos ut sed atque aliquid.
-        Repudiandae architecto mollitia atque. Consectetur iste nesciunt ipsum, inventore delectus quidem minus excepturi ad quia, quisquam voluptatum velit enim a hic deserunt voluptatibus sed! Nostrum minus voluptatum iure vel qui.</H5></div>
+            <h3>HAKKIMIZDA</h3>
+            <H5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, facilis maiores! Sed tempore
+                corrupti sunt reprehenderit, odio aliquam similique obcaecati accusantium minima nam. Dolore, harum
+                dolorum nobis voluptatem quae assumenda.
+                Numquam nesciunt blanditiis dolores fuga, accusamus, laudantium at ab reprehenderit, est animi sapiente
+                vel non modi tempore repellendus? Officia labore eius repellendus consequuntur recusandae perspiciatis
+                numquam dicta hic libero nisi.
+                Laboriosam nihil alias enim sed. Dolores ab temporibus delectus fuga in, dignissimos necessitatibus
+                harum, ipsam nemo laboriosam maiores magni, aperiam commodi quasi iusto quo? Voluptatibus eos ut sed
+                atque aliquid.
+                Repudiandae architecto mollitia atque. Consectetur iste nesciunt ipsum, inventore delectus quidem minus
+                excepturi ad quia, quisquam voluptatum velit enim a hic deserunt voluptatibus sed! Nostrum minus
+                voluptatum iure vel qui.</H5>
+        </div>
         <div class="flex col-3">
             <div class="d-flex ms-4 mt-4">
-            <a href="" class="btn btn-foother"><img src="instagram.png" alt=""></a>
-            <a href="" class="btn btn-foother"><img src="facebook-app-symbol.png" alt=""></a>
-            <a href="" class="btn btn-foother"><img src="youtube.png" alt=""></a>
-        </div>
-        <div class="d-flex">
-           <img src="logo.png" style="width: 400px;height: auto"  alt="">
-        </div>
+                <a href="" class="btn btn-foother"><img src="instagram.png" alt=""></a>
+                <a href="" class="btn btn-foother"><img src="facebook-app-symbol.png" alt=""></a>
+                <a href="" class="btn btn-foother"><img src="youtube.png" alt=""></a>
+            </div>
+            <div class="d-flex">
+                <img src="logo.png" style="width: 400px;height: auto" alt="">
+            </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <div class="overlay" id="overlay"></div>
 
     <script>
         $(document).ready(function() {
@@ -223,18 +246,41 @@
                     navbar.removeClass("bg-color1");
                 }
             });
+
+
         });
+        $(document).ready(function() {
+
+            $(".card1").animate({
+                top: 0
+            }, 1000);
+        });
+        $(document).ready(function() {
+            $("#search").click(function() {
+                console.log("merhaba dünya");
+                $("#overlay").show().addClass("blur");
+                document.getElementById("card").setAttribute("style", "display: block;");
+                $("#card").animate({
+                    top: 0
+                }, 1000);
+            });
+
+            $(document).on("click", function(event) {
+    if (!$(event.target).closest("#card, #search").length) {
+      $("#card").hide();
+      $("#overlay").hide().removeClass("blur");
+    }
+  });
+
+  // Kartın içindeki elemanlara tıklama olayını engellemek için
+  $("#card").on("click", function(e) {
+    e.stopPropagation();
+  });
+        });
+
+
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
